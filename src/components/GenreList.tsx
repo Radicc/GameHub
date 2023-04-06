207c0e1fa092ef537f7c5d4a9f2b88240021b866
Radicc <tomas.radic00@gmail.com>
2023-04-06 20:31:59 +0200

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,21 +1,39 @@
+import MMOARPG_img from "../assets/Geners Images/MMOARPG.jpg";
+import MOBA_img from "../assets/Geners Images/MOBA.jpg";
+import CardGame_img from "../assets/Geners Images/CARDGAME.jpg";
+import Shooter_img from "../assets/Geners Images/SHOOTER.jpg";
+import Strategy_img from "../assets/Geners Images/STRATEGY.jpg";
+import Racing_img from "../assets/Geners Images/RACING.jpg";
+import Fighting_img from "../assets/Geners Images/FIGHTING.jpg";
+import Social_img from "../assets/Geners Images/SOCIAL.jpg";
+import Sports_img from "../assets/Geners Images/SPORTS.jpg";
+import BattleRoyale_img from "../assets/Geners Images/BATTLEROYALE.jpg";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+
 const GenreList = () => {
   const genres = [
-    "MMORPG",
-    "Shooter",
-    "Anime",
-    "Strategy",
-    "Fantasy",
-    "Sci-Fi",
-    "Racing",
-    "Social",
-    "Sports",
+    { name: "RPG", img: MMOARPG_img },
+    { name: "MOBA", img: MOBA_img },
+    { name: "Shooter", img: Shooter_img },
+    { name: "Strategy", img: Strategy_img },
+    { name: "Card Game", img: CardGame_img },
+    { name: "Fighting", img: Fighting_img },
+    { name: "Racing", img: Racing_img },
+    { name: "Social", img: Social_img },
+    { name: "Sports", img: Sports_img },
+    { name: "Battle Royale", img: BattleRoyale_img },
   ];
   return (
-    <ul>
+    <List>
       {genres.map((ganre) => (
-        <li key={ganre}>{ganre}</li>
+        <ListItem paddingY="5px" key={ganre.name}>
+          <HStack>
+            <Image borderRadius={8} boxSize="40px" src={ganre.img} />
+            <Text fontSize="lg">{ganre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
