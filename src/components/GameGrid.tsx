5a22a93831610a6439738805a8ade2053ccae406
Radicc <tomas.radic00@gmail.com>
2023-04-09 13:17:13 +0200

    Syntax

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,37 +14,34 @@ const GameGrid = ({ gameQuery }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   let isVisible = false;
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding={5}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
 
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding={5}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-
-        {data.map((game) =>
-          !gameQuery.searchText ? (
+      {data.map((game) =>
+        !gameQuery.searchText ? (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ) : (
+          game.title.toLocaleLowerCase().includes(gameQuery.searchText) && (
             <GameCardContainer key={game.id}>
               <GameCard game={game} />
             </GameCardContainer>
-          ) : (
-            game.title.toLocaleLowerCase().includes(gameQuery.searchText) && (
-              <GameCardContainer key={game.id}>
-                <GameCard game={game} />
-              </GameCardContainer>
-            )
           )
-        )}
-      </SimpleGrid>
-    </>
+        )
+      )}
+    </SimpleGrid>
   );
 };
 
