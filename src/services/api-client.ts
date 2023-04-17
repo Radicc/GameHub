b84a4ad222071a6a52c2204cfa4a9fda0a43e62f
Radicc <tomas.radic00@gmail.com>
2023-04-17 15:59:09 +0200

    newApiKey

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,7 +4,7 @@ export default axios.create({
   method: "GET",
   baseURL: "https://free-to-play-games-database.p.rapidapi.com/api",
   headers: {
-    "X-RapidAPI-Key": "b6fa1d0134msh24115e0ee988c0dp18178fjsndbf50438b615",
+    "X-RapidAPI-Key": "29a0da5e72msh50afe1dd575047bp1cb395jsnbb31b48a6817",
     "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
   },
 });
